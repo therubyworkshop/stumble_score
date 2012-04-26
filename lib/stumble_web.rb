78238aef475d7b4938c7c86c593e22a05ce18be7
Greg Sterndale <gsterndale@gmail.com>
2012-04-25 22:11:25 -0400

    Web app displays number of nearby bars

--- lib/stumble_web.rb
+++ lib/stumble_web.rb
@@ -5,12 +5,15 @@ require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 get '/' do
   address = params[:address]
   if address && address.length > 1
+    location        = StumbleScore::Location.new(address)
+    bar_count       = location.bar_count
     "<!doctype html>
     <html>
     <head></head>
     <body>
       <h1>Welcome to StumbleScore!</h1>
       <p>Calculating stumble score for #{address}.</p>
+      <p>Bar count: #{bar_count}</p>
     </body>
     </html>"
   else
---
+++ test/test_10_web_bar_count.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.join('..', 'test_helper'), __FILE__)
+require "rubygems"
+require 'rack/test'
+require File.expand_path(File.join('..', '..', 'lib', 'stumble_web'), __FILE__)
+
+ENV['RACK_ENV'] = 'test'
+
+class TestStumbleWeb < Test::Unit::TestCase
+  include LocationFixtures
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_page_displays_number_of_bars_near_specified_address
+    get "/?address=#{URI.escape(CHESTNUT_HILL)}"
+    assert last_response.ok?, "Request failed!"
+    assert last_response.body.include?("Bar count: #{CHESTNUT_HILL_BAR_COUNT}"),
+      "TODO display number of bars on page for specified address."
+
+    get "/?address=#{URI.escape(PAOLI)}"
+    assert last_response.ok?, "Request failed!"
+    assert last_response.body.include?("Bar count: #{PAOLI_BAR_COUNT}"),
+      "That's odd. The specified number of bars works for one address but not another."
+
+    get "/?address=#{URI.escape(RITTENHOUSE)}"
+    assert last_response.ok?, "Request failed!"
+    assert last_response.body.include?("Bar count: #{RITTENHOUSE_BAR_COUNT}"),
+      "That's odd. The specified number of bars works in two addresses but not a third."
+  end
+
+end
+
