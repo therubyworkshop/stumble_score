1776af676537f908bd415b835e56ffb020e6fca4
Greg Sterndale <gsterndale@gmail.com>
2012-04-25 22:43:14 -0400

    Web app displays list of bar names near specified address

---
+++ test/test_13_web_bar_names.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.join('..', 'test_helper'), __FILE__)
+require "rubygems"
+require 'rack/test'
+require File.expand_path(File.join('..', '..', 'lib', 'stumble_web'), __FILE__)
+
+ENV['RACK_ENV'] = 'test'
+
+class TestStumbleWeb < Test::Unit::TestCase
+  include LocationFixtures
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_page_displays_bar_names_near_specified_address
+    get "/?address=#{URI.escape(CHESTNUT_HILL)}"
+    assert last_response.ok?, "Request failed!"
+    assert last_response.body.include?(CHESTNUT_HILL_BAR),
+      "TODO display bars on page near specified address."
+
+    get "/?address=#{URI.escape(PAOLI)}"
+    assert last_response.ok?, "Request failed!"
+    assert last_response.body.include?(PAOLI_BAR),
+      "That's odd. The bar list works for one address but not another."
+
+    get "/?address=#{URI.escape(RITTENHOUSE)}"
+    assert last_response.ok?, "Request failed!"
+    assert last_response.body.include?(RITTENHOUSE_BAR),
+      "That's odd. The bar list works in two addresses but not a third."
+  end
+
+end
+
