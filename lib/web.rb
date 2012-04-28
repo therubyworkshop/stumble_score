bb0520de02f861da0b06235511f1d05862e84379
Greg Sterndale <gsterndale@gmail.com>
2012-04-28 00:19:24 -0400

    HTML placeholder in web interface

--- lib/web.rb
+++ lib/web.rb
@@ -3,6 +3,12 @@ require "sinatra"
 require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 
 get '/' do
-  "Hello from the Web Interface"
+  "<!doctype html>
+  <html>
+  <head></head>
+  <body>
+    <h1>Hello from the Web Interface</h1>
+  </body>
+  </html>"
 end
 
