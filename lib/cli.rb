89dc1fb1f55b2ac65d5c037105762417ee26ae85
Greg Sterndale <gsterndale@gmail.com>
2012-04-27 23:22:46 -0400

    03 cli usage

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,8 +3,13 @@ require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 class CLI
 
   def self.run(address="")
-    "Welcome to StumbleScore!\n" \
-    "Calculating stumble score for \"#{address}\"."
+    if address.length > 1
+      "Welcome to StumbleScore!\n" \
+      "Calculating stumble score for \"#{address}\"."
+    else
+      "Usage: stumble_score <location> \n" \
+      " e.g.: stumble_score 19118"
+    end
   end
 
 end
