71da1ef1cb075413998983c5667be5824fee811d
Greg Sterndale <gsterndale@gmail.com>
2012-04-27 23:13:23 -0400

    01 cli greeting

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 class CLI
 
   def self.run(address="")
-    "Hello from the Command Line Interface"
+    "Welcome to StumbleScore!"
   end
 
 end
