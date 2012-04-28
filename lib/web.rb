40d594f4ec0e7968f3578649cba639a4e63b5537
Greg Sterndale <gsterndale@gmail.com>
2012-04-28 00:28:56 -0400

    10 web usage

--- lib/web.rb
+++ lib/web.rb
@@ -4,13 +4,24 @@ require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 
 get '/' do
   address = params[:address]
-  "<!doctype html>
-  <html>
-  <head></head>
-  <body>
-    <h1>Welcome to StumbleScore!</h1>
-    <p>Calculating stumble score for #{address}.</p>
-  </body>
-  </html>"
+  if address && address.length > 1
+    "<!doctype html>
+    <html>
+    <head></head>
+    <body>
+      <h1>Welcome to StumbleScore!</h1>
+      <p>Calculating stumble score for #{address}.</p>
+    </body>
+    </html>"
+  else
+    "<!doctype html>
+    <html>
+    <head></head>
+    <body>
+      <h1>Welcome to StumbleScore!</h1>
+      <p>An address parameter is required e.g. http://www.stumblescore.com?address=rittenhouse%20square,%20pa</p>
+    </body>
+    </html>"
+  end
 end
 
